579e492e46776648990385d8fe5bf056aa99d047
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-02-22 16:57:52 -0500

    reorganized styles within these files

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Issue = ({issue}) => (
   <li id={issue.id} number={issue.number} className="issue">
-    <div className='card text-center' style={{width: '18rem', padding: '10px', background: '#eee'}}> 
+    <div className='card text-center' style={styles.issue_card}> 
       <div className='card-body'>
-        <h5 className='card-title' style={{fontSize: '17px'}}>{issue.title}</h5>
-        <h6 className='card-subtitle mb-2 price' style={{fontSize: '14px'}}>{`$${issue.price}`}</h6>
+        <h5 className='card-title' style={styles.issue_title}>{issue.title}</h5>
+        <h6 className='card-subtitle mb-2 price' style={styles.issue_price}>{`$${issue.price}`}</h6>
         <p className='card-text'
-          style={{fontSize: '14px'}}
+          style={styles.issue_assignee}
         >
           {issue.stage === 'active' ? <p>Assigned to: {issue.assignee}</p> : ''}
         </p>
-        <span className='card-text text-muted' style={{fontSize: '12px'}}> Due on: {issue.due_date}</span>
+        <span className='card-text text-muted' style={styles.issue_due_date}> 
+          Due on: {issue.due_date}
+        </span>
       </div>
-      <div className='card-footer text-center' style={{fontSize: '12px', background: 'none'}}>
+      <div className='card-footer text-center' style={styles.issue_card_footer}>
         <Link to={issue.html_url} target="_blank">
           <span className="">View on GitHub</span>
         </Link>
@@ -22,5 +24,29 @@ const Issue = ({issue}) => (
     </div>
   </li>
 );
+
+var styles = {
+  issue_card: {
+    width: '18rem',
+    padding: '10px',
+    background: '#eee'
+  },
+  issue_title: {
+    fontSize: '17px'
+  },
+  issue_price: {
+    fontSize: '14px'
+  },
+  issue_assignee: {
+    fontSize: '14px'
+  },
+  issue_due_date: {
+    fontSize: '12px'
+  },
+  issue_card_footer: {
+    fontSize: '12px',
+    background: 'none'
+  }
+}
 
 export default Issue;
--- client/src/components/Repo.js
+++ client/src/components/Repo.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Repo = ({repo}) => (
   <li id={repo.id} className="repo">
-    <div className="card text-center" style={styles.repo}>
+    <div className="card text-center" style={styles.repo_card}>
       <div className="card-body repo-title">
         <Link to={repo.html_url} target="_blank" style={styles.repo_title}>
           <h5 className="card-title">{repo.name}</h5>
@@ -12,9 +12,9 @@ const Repo = ({repo}) => (
         <p className="card-text">{repo.description}</p>
       </div>
     </div>
-    <div className="card-footer text-center" style={{background: '#222'}}>
+    <div className="card-footer text-center" style={styles.repo_card_footer}>
         <Link to={`/${repo.owner.login}/${repo.name}/issues`}
-          className="card-link" style={{color: '#eee'}}
+          className="card-link" style={styles.repo_view_issues}
         >
           View Issues
         </Link>
@@ -23,23 +23,29 @@ const Repo = ({repo}) => (
 );
 
 const styles = {
-  truncate: {
-    fontSize: '12px',
-    width: '100%',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis'
-  },
-  repo_title: {
-    textDecoration: 'none',
-  },
-  repo: {
+  repo_card: {
     width: '18rem',
     padding: '10px',
     maxHeight: '135px',
     minHeight: '135px',
     fontSize: '14px'
+  },
+  repo_title: {
+    textDecoration: 'none',
+  },
+  repo_card_footer: {
+    background: '#222'
+  },
+  repo_view_issues: {
+    color: '#eee'
   }
 }
+ // truncate: {
+  //   fontSize: '12px',
+  //   width: '100%',
+  //   whiteSpace: 'nowrap',
+  //   overflow: 'hidden',
+  //   textOverflow: 'ellipsis'
+  // }
 
 export default Repo;
